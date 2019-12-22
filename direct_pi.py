f68c627ab1858c516c2146457f6103b1b4293993
Joseph Fahnestock <joeyfahnestock@gmail.com>
2019-12-22 03:53:45 -0600

    Added direct_pi.py

---
+++ direct_pi.py
@@ -0,0 +1,23 @@
+# direct_pi.py
+# joseph fahnestock
+# an interpretation of the program written in pseudocode on page 3
+# of Statistical Mechanics by Werner Krauth
+
+from numpy import random as rnd # import random module from numpy
+
+trials = int(input("How many trials would you like to run? "))
+completed_trials = 0
+hits = 0
+
+while completed_trials < trials:
+    # Complete number of trials user requested
+    # test whether the (x,y) is within a unit circle
+    x = rnd.random() # x: [-1, 1)
+    y = rnd.random() # y: [-1, 1)
+    if (x**2 + y**2) < 1:
+        hits += 1
+    completed_trials += 1
+
+pi = 4*(float(hits)/completed_trials)
+print("There were " + str(hits) + " hits. The resultant approximation for pi is " + str(pi))
+
